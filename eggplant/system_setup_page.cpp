1660c9df536086ab1a79e6ee92cd844828b7d626
Slash.Huang <slash.huang@dfi.com>
2021-09-30 21:33:57 +0800

    update system set

--- eggplant/system_setup_page.cpp
+++ eggplant/system_setup_page.cpp
@@ -102,7 +102,7 @@ static int sys_data_title_x[15] = {170, 170, 170, 170, 245, 285, 170, 170, 270, 
 static int sys_data_title_y[15] = {100, 100, 100, 55,  55,  55 , 95,  135, 135, 135, 135, 100, 100};
 
 /*                               0    1    2    3    4    5    6    7    8    9    10   11 */
-static int sys_data_msg_x[15] = {160, 240, 170, 220, 260, 300, 220, 220, 300, 360, 220, 260};
+static int sys_data_msg_x[15] = {260, 240, 240, 220, 260, 300, 220, 220, 300, 360, 220, 260};
 static int sys_data_msg_y[15] = {100, 100, 100, 55,  55,  55 , 95,  135, 135, 135, 100, 100};
 
 void SystemSetup_Page::Enable_Icon_Light(int i)
@@ -155,38 +155,9 @@ SystemSetup_Page::SystemSetup_Page(QWidget *parent) : Frame_Page(parent)
         str_len_data = sys_msg_data[i].length() * 20;
         show_item_data[i] = new Show_text(this);
         show_item_data[i]->set_text(sys_msg_data[i]);
-        show_item_data[i]->setGeometry(sys_data_msg_x[i], sys_data_msg_y[i], str_len_data, 50);
+        show_item_data[i]->setGeometry(sys_data_msg_x[i], sys_data_msg_y[i], str_len_data + 10, 50);
         show_item_data[i]->hide();
     }
-
-//    show_item[1]->show();
-//    show_item_data[1]->show();
-
-
-//    show_item[SYS_DATA_ITEM_TIME]->show();
-//    show_item[SYS_DATA_ITEM_TIME_SP1]->show();
-//    show_item[SYS_DATA_ITEM_TIME_SP2]->show();
-//    show_item_data[SYS_DATA_MSG_TIME_H]->show();
-//    show_item_data[SYS_DATA_MSG_TIME_M]->show();
-//    show_item_data[SYS_DATA_MSG_TIME_S]->show();
-
-//    show_item[SYS_DATA_ITEM_WEEK]->show();
-//    show_item[SYS_DATA_ITEM_DATE]->show();
-//    show_item[SYS_DATA_ITEM_YEAR]->show();
-//    show_item[SYS_DATA_ITEM_MONTH]->show();
-//    show_item[SYS_DATA_ITEM_DAY]->show();
-//
-//    show_item_data[SYS_DATA_MSG_WEEK]->show();
-//    show_item_data[SYS_DATA_MSG_YEAR]->show();
-//    show_item_data[SYS_DATA_MSG_MONTH]->show();
-//    show_item_data[SYS_DATA_MSG_DAY]->show();
-
-//    show_item[SYS_DATA_ITEM_LIGHT]->show();
-//    show_item_data[SYS_DATA_MSG_LIGHT]->show();
-
-//    show_item[SYS_DATA_ITEM_FACTORY]->show();
-//    show_item_data[SYS_DATA_MSG_FACTORY]->show();
-
 }
 
 void SystemSetup_Page::paintEvent(QPaintEvent *)
@@ -202,8 +173,9 @@ void SystemSetup_Page::paintEvent(QPaintEvent *)
 void SystemSetup_Page::GetMcuData(class CarInfo_Data *protolcol_data)
 {
     uint8_t page_data[128];
-    uint8_t u8_data_tmp;
+    uint8_t u8_data_tmp, u8_data_left;
     QString str_tmp;
+
     int i, data_index;
     #define PROTOCOL_OFF_ID0_ALL_WHITE 0
     #define PROTOCOL_OFF_ID1_DATE_MILEAGE 1
@@ -216,6 +188,14 @@ void SystemSetup_Page::GetMcuData(class CarInfo_Data *protolcol_data)
     memcpy(page_data, protolcol_data->page_data, sizeof(uint8_t) * protolcol_data->page_data_sz);
 
     u8_data_tmp  = page_data[0] > 6 ? 6 : page_data[0];
+
+    for (i = SYS_BTN_ID0_MILEAGE; i < SYS_BTN_END; i++)
+        Disable_Icon_Light(i);
+
+    for (i = SYS_DATA_MSG_MILEAGE; i < SYS_DATA_MSG_END; i++) {
+        //hide data for old dadta
+        show_item_data[i]->hide();
+    }
 
     if (u8_data_tmp != PROTOCOL_OFF_ID0_ALL_WHITE)
         m_btn_id = u8_data_tmp - 1;
@@ -266,112 +246,131 @@ void SystemSetup_Page::GetMcuData(class CarInfo_Data *protolcol_data)
             break;
         }
 
-        show_item_data[data_index]->show();
+        //show_item_data[data_index]->show();
         if (page_data[2] == 1)
             show_item_data[data_index]->text_color = QColor(0, 204, 240);
         else
             show_item_data[data_index]->text_color = Qt::white;
     }
 
-    //Data for Mile
-    u8_data_tmp = page_data[3] & 0x03;
-    if (u8_data_tmp)
-        str_tmp.sprintf("MP/H");
-    else
-        str_tmp.sprintf("MP/H");
+    u8_data_left = u8_data_tmp;
 
-    show_item[SYS_DATA_MSG_MILEAGE]->show();
-    show_item_data[SYS_DATA_MSG_MILEAGE]->set_text(str_tmp);
-    show_item_data[SYS_DATA_MSG_MILEAGE]->show();
+    //Data for Mile
+    if (u8_data_left == PROTOCOL_OFF_ID1_DATE_MILEAGE) {
+        u8_data_tmp = page_data[3] & 0x03;
+        if (u8_data_tmp)
+            str_tmp.sprintf("MP/H");
+        else
+            str_tmp.sprintf("KM/H");
+
+        show_item[SYS_DATA_MSG_MILEAGE]->show();
+        show_item_data[SYS_DATA_MSG_MILEAGE]->set_text(str_tmp);
+        show_item_data[SYS_DATA_MSG_MILEAGE]->show();
+        return;
+    }
 
     //Data for Alarm
-    u8_data_tmp = (page_data[3] & (0x0F << 2)) >> 2;
-    if (u8_data_tmp == 0)
-        str_tmp.sprintf("1");
-    else if (u8_data_tmp == 1)
-        str_tmp.sprintf("2");
-    else
-        str_tmp.sprintf("3");
+    if (u8_data_left == PROTOCOL_OFF_ID2_DATE_ALARM) {
+        u8_data_tmp = (page_data[3] & (0x0F << 2)) >> 2;
+        if (u8_data_tmp == 0)
+            str_tmp.sprintf("1");
+        else if (u8_data_tmp == 1)
+            str_tmp.sprintf("2");
+        else
+            str_tmp.sprintf("3");
 
-    show_item[SYS_DATA_MSG_ALARM]->show();
-    show_item_data[SYS_DATA_MSG_ALARM]->set_text(str_tmp);
-    show_item_data[SYS_DATA_MSG_ALARM]->show();
+        show_item[SYS_DATA_MSG_ALARM]->show();
+        show_item_data[SYS_DATA_MSG_ALARM]->set_text(str_tmp);
+        show_item_data[SYS_DATA_MSG_ALARM]->show();
+        return;
+    }
 
     //Data for Btn Volume
-    u8_data_tmp = (page_data[3] & (0x01 << 6)) >> 6;
-    if (u8_data_tmp == 0)
-        str_tmp.sprintf("OFF");
-    else
-        str_tmp.sprintf("ON");
+    if (u8_data_left == PROTOCOL_OFF_ID3_DATE_BTNVOLUM) {
+        u8_data_tmp = (page_data[3] & (0x01 << 6)) >> 6;
+        if (u8_data_tmp == 0)
+            str_tmp.sprintf("OFF");
+        else
+            str_tmp.sprintf("ON");
 
-    show_item[SYS_DATA_MSG_BTNVOLUM]->show();
-    show_item_data[SYS_DATA_MSG_BTNVOLUM]->set_text(str_tmp);
-    show_item_data[SYS_DATA_MSG_BTNVOLUM]->show();
-
+        show_item[SYS_DATA_MSG_BTNVOLUM]->show();
+        show_item_data[SYS_DATA_MSG_BTNVOLUM]->set_text(str_tmp);
+        show_item_data[SYS_DATA_MSG_BTNVOLUM]->show();
+        return;
+    }
 
     //Year-Date-Time
-    show_item[SYS_DATA_ITEM_TIME]->show();
-    show_item[SYS_DATA_ITEM_TIME_SP1]->show();
-    show_item[SYS_DATA_ITEM_TIME_SP2]->show();
-    show_item[SYS_DATA_ITEM_WEEK]->show();
-    show_item[SYS_DATA_ITEM_DATE]->show();
-    show_item[SYS_DATA_ITEM_YEAR]->show();
-    show_item[SYS_DATA_ITEM_MONTH]->show();
-    show_item[SYS_DATA_ITEM_DAY]->show();
+    if (u8_data_left == PROTOCOL_OFF_ID4_DATE_TIME) {
+        show_item[SYS_DATA_ITEM_TIME]->show();
+        show_item[SYS_DATA_ITEM_TIME_SP1]->show();
+        show_item[SYS_DATA_ITEM_TIME_SP2]->show();
+        show_item[SYS_DATA_ITEM_WEEK]->show();
+        show_item[SYS_DATA_ITEM_DATE]->show();
+        show_item[SYS_DATA_ITEM_YEAR]->show();
+        show_item[SYS_DATA_ITEM_MONTH]->show();
+        show_item[SYS_DATA_ITEM_DAY]->show();
+    
+        //Data for Year
+        u8_data_tmp = page_data[9];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_YEAR]->set_text(str_tmp);
 
-    //Data for Year
-    u8_data_tmp = page_data[9];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_YEAR]->set_text(str_tmp);
+        //Data for Month
+        u8_data_tmp = page_data[8];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_MONTH]->set_text(str_tmp);
 
-    //Data for Month
-    u8_data_tmp = page_data[8];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_MONTH]->set_text(str_tmp);
+        //Data for Day
+        u8_data_tmp = page_data[7];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_DAY]->set_text(str_tmp);
 
-    //Data for Day
-    u8_data_tmp = page_data[7];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_DAY]->set_text(str_tmp);
+        //Data for Time-H
+        u8_data_tmp = page_data[6];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_TIME_H]->set_text(str_tmp);
 
-    //Data for Time-H
-    u8_data_tmp = page_data[6];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_TIME_H]->set_text(str_tmp);
+        //Data for Time-M
+        u8_data_tmp = page_data[5];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_TIME_M]->set_text(str_tmp);
 
-    //Data for Time-M
-    u8_data_tmp = page_data[5];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_TIME_M]->set_text(str_tmp);
+        //Data for Time-S
+        u8_data_tmp = page_data[4];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_TIME_S]->set_text(str_tmp);
 
-    //Data for Time-S
-    u8_data_tmp = page_data[4];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_TIME_S]->set_text(str_tmp);
-
-    //Data for Week
-    u8_data_tmp = page_data[10];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_WEEK]->set_text(str_tmp);
+        //Data for Week
+        u8_data_tmp = page_data[10];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_WEEK]->set_text(str_tmp);
+        
+        show_item_data[SYS_DATA_MSG_YEAR]->show();
+        show_item_data[SYS_DATA_MSG_MONTH]->show();
+        show_item_data[SYS_DATA_MSG_DAY]->show();
+        show_item_data[SYS_DATA_MSG_TIME_H]->show();
+        show_item_data[SYS_DATA_MSG_TIME_M]->show();
+        show_item_data[SYS_DATA_MSG_TIME_S]->show();
+        show_item_data[SYS_DATA_MSG_WEEK]->show();
+        return;
+    }
 
     //Data for Light
-    u8_data_tmp = page_data[11];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_LIGHT]->set_text(str_tmp);
+    if (u8_data_left == PROTOCOL_OFF_ID5_DATE_LIGHT) {
+        u8_data_tmp = page_data[11];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_LIGHT]->set_text(str_tmp);
+        show_item_data[SYS_DATA_MSG_LIGHT]->show();
+        return;
+    }
 
     //Data for Factory
-    u8_data_tmp = page_data[12];
-    str_tmp.sprintf("%d", u8_data_tmp);
-    show_item_data[SYS_DATA_MSG_FACTORY]->set_text(str_tmp);
-
-    show_item_data[SYS_DATA_MSG_FACTORY]->show();
-    show_item_data[SYS_DATA_MSG_LIGHT]->show();
-    show_item_data[SYS_DATA_MSG_WEEK]->show();
-    show_item_data[SYS_DATA_MSG_TIME_S]->show();
-    show_item_data[SYS_DATA_MSG_TIME_M]->show();
-    show_item_data[SYS_DATA_MSG_TIME_H]->show();
-    show_item_data[SYS_DATA_MSG_DAY]->show();
-    show_item_data[SYS_DATA_MSG_MONTH]->show();
-    show_item_data[SYS_DATA_MSG_YEAR]->show();
+    if (u8_data_left == PROTOCOL_OFF_ID6_DATE_FACTORY) {
+        u8_data_tmp = page_data[12];
+        str_tmp.sprintf("%d", u8_data_tmp);
+        show_item_data[SYS_DATA_MSG_FACTORY]->set_text(str_tmp);
+        show_item_data[SYS_DATA_MSG_FACTORY]->show();
+        return;
+    }
 
 }
