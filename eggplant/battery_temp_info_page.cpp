f43bd179613011ba826ca5642fc2e8401ab94a7f
xlloss <xlloss@gmail.com>
2021-10-26 00:50:58 +0800

    fixed system date, time, factory relation issue

--- eggplant/battery_temp_info_page.cpp
+++ eggplant/battery_temp_info_page.cpp
@@ -182,7 +182,7 @@ void BatTempInfo_Page::GetMcuData(class CarInfo_Data *protolcol_data)
         /* HI temperature */
         d_data_tmp = double(page_data[j + 1] << 8 | page_data[j]);
         d_data_tmp = (d_data_tmp * 0.1) - 40;
-        else if (d_data_tmp < -40 )
+        if (d_data_tmp < -40 )
             d_data_tmp = -40;
 
         str_tmp.sprintf("%.1f °C", d_data_tmp);
@@ -191,9 +191,7 @@ void BatTempInfo_Page::GetMcuData(class CarInfo_Data *protolcol_data)
         /* LO temperature */
         d_data_tmp = double(page_data[j + 16 + 1] << 8 | page_data[j + 16]);
         d_data_tmp = (d_data_tmp * 0.001);
-        if (d_data_tmp > 32.767 )
-            d_data_tmp = 32.767;
-        else if (d_data_tmp < 0 )
+        if (d_data_tmp < 0 )
             d_data_tmp = 0;
 
         str_tmp.sprintf("%.1f °C", d_data_tmp);
